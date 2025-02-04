9ed65cba189a647a73f10efc638dd0f4eba06191
Mustapha tahiru <tahirumustapha13@gmail.com>
2025-02-04 10:24:24 +0000

    ecommerce projectclear

---
+++ src/helpers/displayCurrency.js
@@ -0,0 +1,12 @@
+const displayINRCurrency = (num) => {
+    const formatter = new Intl.NumberFormat('en-GH',{
+        style : "currency",
+        currency : 'GHS',
+        minimumFractionDigits : 2
+    })
+
+    return formatter.format(num)
+
+}
+
+export default displayINRCurrency
